2a04a3f115558ea013dfb73eec8aa4e45a215db8
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2025-04-12 15:26:48 -0400

    feat: implements update, find and findAll on order repository with tests

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -47,6 +47,10 @@ export default class Order {
         return true;
     }
 
+    addItem(item: OrderItem): void {
+        this._items.push(item);
+    }
+
     total(): number {
         return this._items.reduce((acc, item) => acc + item.orderItemTotal(), 0);
     }
